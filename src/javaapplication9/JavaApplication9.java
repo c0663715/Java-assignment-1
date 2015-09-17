d5301ae3003274b23577aec631ca9cfa3228c933
Gurvinder sing <C0663715@mylambton.ca>
2015-09-16 23:07:19 -0400

    The assignment content is added

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author c0663715
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Job posting 
+        //Java Developer http:www.indeed.ca/cmp/CoreTech-Staffing-and-Professional-Services/jobs/Java-Developer-0f659386e659af0b?q=Java
+
+        //WEB2 technologies - World Wide Web 2.0 - https:en.wikipedia.org/wiki/Web_2.0
+        //JavaScript - Client side scripting language- https:en.wikipedia.org/wiki/JavaScript
+        //Typescript - Open source programming developed by microsoft - https:en.wikipedia.org/wiki/TypeScript
+        //HTML - HuperText Markup Language - https:en.wikipedia.org/wiki/HTML
+        //CSS - Cascading style sheets language - https:en.wikipedia.org/wiki/Cascading_Style_Sheets
+        //Unix - Multitasking, multiuser computer operating system - https:en.wikipedia.org/wiki/Unix
+        //Linux - Free and open-source computer operating system - https:en.wikipedia.org/wiki/Linux
+        //KDB -Kernal Debugger - https:en.wikipedia.org/wiki/KDB
+        //Scala - Programming for general software applications - https:en.wikipedia.org/wiki/Scala_(programming_language)
+        //Toronto Salary: 72k
+        //Vancouver Salary: 76k
+        //Waterloo Salary: 75k       
+    }
+
+}
